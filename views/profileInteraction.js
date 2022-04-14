477a273abfa8b286a2e6959825ab03aff2157c12
Vadims Lapins <t1lava01@students.oamk.fi>
2022-04-14 06:30:31 +0300

    Created leveling up animation

--- views/profileInteraction.js
+++ views/profileInteraction.js
@@ -43,6 +43,53 @@ function addEnterKeyListener() {
         }
     });
 }
-
+function visualLevelUp() {
+    //track how much time function is taking
+    let startTime = new Date();
+    skillName = $(".modal").find("h1").text().toLowerCase();
+    targetSkill = $(".element."+skillName);
+    levelUpSign = $(targetSkill).find(".levelUpFlexContainer");
+    animationDuration = ($(".levelUpTransition").css("transition").split(" ")[1].split("s")[0])*1000
+    animationDurationFlex = ($(".levelUpFlex").css("transition").split(" ")[1].split("s")[0])*1000
+    //select all child elements of targetSkill except element with class "levelUpFlexContainer"
+    targetSkill.children().not(".levelUpFlexContainer").each(function() {
+        let targetElement = $(this);
+        targetElement.toggleClass("levelUpTransition")
+        targetElement.toggleClass("levelUpOpacity")
+    });
+    setTimeout(function() {
+        targetSkill.toggleClass("animatedElement")
+        targetSkill.toggleClass("levelUpTransition")
+    }, animationDuration);
+    setTimeout(function() {
+        levelUpSign.toggleClass("levelUpFlex")
+    }, animationDuration);
+    setTimeout(function() {
+        setTimeout(function() {
+            levelUpSign.toggleClass("levelUpFlex")
+        }, animationDurationFlex);
+        setTimeout(function() {
+            targetSkill.toggleClass("animatedElement")
+            setTimeout(function() {
+                targetSkill.toggleClass("levelUpTransition")
+                setTimeout(function() {
+                    targetSkill.children().not(".levelUpFlexContainer").each(function() {
+                        let targetElement = $(this);
+                        targetElement.toggleClass("levelUpOpacity")
+                    })
+                    setTimeout(function() {
+                        targetSkill.children().not(".levelUpFlexContainer").each(function() {
+                            let targetElement = $(this);
+                            targetElement.toggleClass("levelUpTransition")
+                            let endTime = new Date();
+                            let timeTaken = endTime - startTime;
+                            $(".elements").attr("data-timeTakenForLvlUp", timeTaken)
+                        });
+                    }, animationDuration/2);;
+                }, animationDuration);
+            }, animationDuration);
+        }, animationDurationFlex);
+    }, animationDuration);
+}
 addEventListener("click", openModal);
 addEventListener("click", clickOutsideModal);
